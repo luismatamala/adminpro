41256f35205b66aeefb1cc6696c97477f4b838bd
Luis Matamala <lmatamala@digevo.com>
2019-09-13 14:18:59 -0300

    nueva pagina de modulos

---
+++ src/app/pages/page.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { PagesRoutingModule } from './pages-routing.module';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+    ],
+    imports: [
+        SharedModule,
+        PagesRoutingModule
+    ]
+})
+
+export class PagesModule {}
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,29 @@
+import { Routes, RouterModule } from "@angular/router";
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { NgModule } from '@angular/core';
+
+
+
+
+const routes: Routes = [
+    {
+      path: '', 
+      component: PagesComponent,
+      children : [
+        {path: 'dashboard', component: DashboardComponent},
+        {path: 'progress', component: ProgressComponent},
+        {path: 'graficas1', component: Graficas1Component},
+        {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  export class PagesRoutingModule { }
+  
